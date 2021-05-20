27b1104f18db385d14d5254213e8c302787d7452
Luis Alfredo <lahoising78@gmail.com>
2021-05-20 02:14:51 -0400

    render a triangle (the bad way)

--- src/mr_platform/mr_gl_renderer.cpp
+++ src/mr_platform/mr_gl_renderer.cpp
@@ -18,17 +18,38 @@ struct Renderer::gui_init_info_s
     char glVersion[16];
 };
     
+static GLuint vertexArrayId;
+static GLuint vertexBuffer;
+
 GlRenderer::GlRenderer()
 {
     #ifdef MOBREND_GLFW_WINDOW
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     #endif
     glClearColor(0.8f, 0.3f, 0.2f, 1.0f);
+
+    glGenVertexArrays(1, &vertexArrayId);
+    glBindVertexArray(vertexArrayId);
+
+    const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+    };
+
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(
+        GL_ARRAY_BUFFER, 
+        sizeof(g_vertex_buffer_data), 
+        g_vertex_buffer_data,
+        GL_STATIC_DRAW);
 }
 
 GlRenderer::~GlRenderer()
 {
-    
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteVertexArrays(1, &vertexArrayId);
 }
 
 void GlRenderer::SetViewport(int x, int y, int width, int height) 
@@ -39,6 +60,20 @@ void GlRenderer::SetViewport(int x, int y, int width, int height)
 void GlRenderer::OnRenderBegin()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        0,
+        nullptr
+    );
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(0);
 }
 
 void GlRenderer::OnRenderEnd()
