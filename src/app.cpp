eedb9b327e85d7918fdb28ed177059fd1749f08d
Luis Chiang <lahoising78@gmail.com>
2021-05-22 23:46:11 -0400

    add mr logger library to project

--- src/app.cpp
+++ src/app.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include "mr_application.h"
+#include "mr_logger.h"
 
 void OnUpdate();
 
@@ -15,6 +16,10 @@ int main(int argc, char *argv[])
 
     mr::Application::GetInstance().Run(params);
     printf("no se destruye?\n");
+
+    mrlog("hey there %d", 77);
+    mrwarn("this is a warning: %s", "just a warning");
+    mrerr("don't do that!");
 
     return 0;
 }
