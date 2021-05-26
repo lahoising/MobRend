5f35403b5b3008f3c1ddb7ea5040a68666529880
Luis Chiang <lahoising78@gmail.com>
2021-05-25 21:25:07 -0400

    fps cam fwd movement

--- src/mr_fps_camera.cpp
+++ src/mr_fps_camera.cpp
@@ -15,11 +15,14 @@ void FPSCamera::Update()
     glm::vec3 position = this->camera.GetPosition();
     glm::quat rotation = this->camera.GetRotation();
     glm::vec3 worldFwd = glm::vec3(0.0f, 0.0f, 1.0f);
+    glm::vec3 worldRight = glm::vec3(1.0f, 0.0f, 0.0f);
     glm::vec3 camFwd = worldFwd * rotation;
+    glm::vec3 camRight = worldRight * rotation;
 
     const float CAM_MOVEMENT_SPEED = 0.1f;
     glm::vec3 deltaMove = glm::vec3(0.0f, 0.0f, 0.0f);
-    deltaMove.x = (float)(input.IsKeyPressed(GLFW_KEY_D) - input.IsKeyPressed(GLFW_KEY_A));
+    deltaMove += camRight * (float)(input.IsKeyPressed(GLFW_KEY_D) - input.IsKeyPressed(GLFW_KEY_A));
+    deltaMove += camFwd * (float)(input.IsKeyPressed(GLFW_KEY_W) - input.IsKeyPressed(GLFW_KEY_S));
     deltaMove *= CAM_MOVEMENT_SPEED;
 
     this->camera.SetPosition(position + deltaMove);
