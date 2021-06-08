645c9dc5bedbee811493d0575e5d4fd47f8ba4cb
Luis Chiang <lahoising78@gmail.com>
2021-06-08 00:14:33 -0400

    recognize variable name and type of first member of struct

--- src/mr_uniform_layout.cpp
+++ src/mr_uniform_layout.cpp
@@ -9,7 +9,7 @@ namespace mr
 static void GetNextWord(const char **source, std::string &outWord)
 {
     const char *nextSpace = *source;
-    while(!isspace(*nextSpace)) nextSpace++;
+    while(isalnum(*nextSpace)) nextSpace++;
 
     outWord.assign(*source, nextSpace - *source);
     *source = nextSpace;
@@ -45,8 +45,11 @@ UniformLayout GetUniformLayout(const char *source)
     const size_t uniformWordLen = strlen("uniform");
     const size_t structWordLen = strlen("struct");
     const char *scanner = source;
+    
     std::string buffer = "";
     buffer.reserve(strlen(scanner));
+    std::string variableName = "";
+    variableName.reserve(64); // too much, but whatever
 
     std::unordered_map<std::string,UniformLayout> shaderStructs;
     std::string structName = "";
@@ -55,23 +58,31 @@ UniformLayout GetUniformLayout(const char *source)
     {
         UniformLayout structLayout;
 
+        /// TODO: what if struct is in the name of a variable?
         scanner += structWordLen;
         while(isspace(*scanner)) scanner++;
         GetNextWord(&scanner, structName);
 
         while(isspace(*scanner) || *scanner == '{') scanner++;
 
+        UniformValue value = {};
+
         // at this point we are at a line and the next two words will be:
         // 1. the uniform type
         // 2. the name of the variable
         GetNextWord(&scanner, buffer);
-        UniformType uniformType = IdentifyUniformType(buffer);
+        value.type = IdentifyUniformType(buffer);
 
         while(isspace(*scanner)) scanner++;
-        GetNextWord(&scanner, buffer);
+        GetNextWord(&scanner, variableName);
+
+        while(isspace(*scanner)) scanner++;
+        if(*scanner == '[')
+        {
+            
+        }
         
-        // at this point we have the name of the variable.
-        // However, it might be appended with brackets and/or semicolons
+        structLayout[variableName] = value;
     }
 
     scanner = source;
