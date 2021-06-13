abf86535e674853412556f3bc480693d7c97784e
Luis Chiang <lahoising78@gmail.com>
2021-06-13 13:00:52 -0400

    move scene code to user app

--- src/app.cpp
+++ src/app.cpp
@@ -3,36 +3,125 @@
 #include "mr_application.h"
 #include "mr_logger.h"
 #include "mr_model.h"
+#include "mr_shader.h"
+#include "mr_fps_camera.h"
+#include "mr_light.h"
 
 class UserApp
 {
 public:
-    UserApp()
-    {
-        // this->model = mr::Model::Load();
-    }
+    UserApp(){}
 
     ~UserApp()
     {
-        // delete(this->model);
+        delete(this->model);
+        delete(this->directional);
+        delete(this->shader);
+    }
+
+    void Start()
+    {
+        mr::DirectionalLight *dirLight = new mr::DirectionalLight(
+            glm::vec3(1.0f, 1.0f, 1.0f), 1.0f
+        );
+        dirLight->direction = glm::normalize(glm::vec3(0.0f, -0.2f, 0.8f));
+        this->directional = dirLight;
+
+        mr::Shader::CreateParams shaderCreateParams = {};
+        shaderCreateParams.vertFilePath = "D:\\Documents\\git\\MobRend\\resources\\default_shader.vert.spv";
+        shaderCreateParams.fragFilePath = "D:\\Documents\\git\\MobRend\\resources\\default_shader.frag.spv";
+        shader = mr::Shader::Create(shaderCreateParams);
+
+        cam = mr::FPSCamera();
+        mr::Camera::Config camConfig = {};
+        camConfig.perspective.fov = glm::radians(60.0f);
+        camConfig.perspective.aspectRatio = 1280.0f / 720.0f;
+        camConfig.perspective.near = 0.1f;
+        camConfig.perspective.far = 1000.0f;
+        cam.camera = mr::Camera(
+            mr::Camera::Type::PERSPECTIVE,
+            camConfig
+        );
+
+        tex = mr::Texture::Create("D:\\Pictures\\Screenshots\\Screenshot (44).png");
+        specMap = mr::Texture::Create("D:\\Documents\\progs\\krita_resources\\MobRend\\IU_Spec.png");
+
+        model = mr::Model::Load("D:\\Documents\\Clases\\4th Year\\sem-2\\it386\\sprints\\3rd\\Lodge_Modular_Pieces_Exploded.fbx");
     }
 
     void Render(mr::Renderer *renderer)
     {
+        glm::mat4 identityMat = glm::identity<glm::mat4>();
+        cam.Update();
 
+        shader->Bind();
+
+        shader->UploadMat4(
+            "u_cam.viewProjection",
+            cam.camera.GetViewProjection()
+        );
+
+        shader->UploadMat4(
+            "u_cam.model",
+            identityMat
+        );
+
+        glm::mat3 normalMat = glm::transpose(glm::inverse(identityMat));
+        shader->UploadMat3(
+            "u_cam.normalMat",
+            normalMat
+        );
+
+        shader->UploadVec3(
+            "u_scene.color",
+            glm::vec3(1.0f, 1.0f, 1.0f)
+        );
+
+        directional->Bind(shader, "u_scene.ambientLight");
+        
+        shader->UploadVec3(
+            "u_scene.viewPos",
+            cam.camera.GetPosition()
+        );
+
+        shader->UploadVec3("u_scene.phongMaterial.diffuse", glm::vec3(1.0f, 0.5f, 0.31f));
+        shader->UploadFloat("u_scene.phongMaterial.diffuseMapStrength", 0.0f);
+        
+        shader->UploadVec3("u_scene.phongMaterial.specular", glm::vec3(0.5f, 0.5f, 0.5f));
+        shader->UploadFloat("u_scene.phongMaterial.specularMapStrength", 0.0f);
+        shader->UploadFloat("u_scene.phongMaterial.shininess", 32.f);
+
+        shader->UploadTexture2D("u_diffuseMap", tex);
+        shader->UploadTexture2D("u_specularMap", specMap);
+
+        mr::Renderer::Command cmd = {};
+        cmd.topologyType = mr::TopologyType::TRIANGLES;
+        cmd.model = model;
+        cmd.renderObjectType = mr::RenderObjectType::MODEL;
+        renderer->Render(cmd);
     }
 
 private:
-    mr::Model *model;
+    mr::Shader *shader = nullptr;
+
+    mr::FPSCamera cam;
+    mr::Texture *tex = nullptr;
+    mr::Texture *specMap = nullptr;
+
+    mr::Light *directional = nullptr;
+
+    mr::Model *model = nullptr;
 };
 UserApp myApp;
 
+void OnStart();
 void OnUpdate();
 void OnRender(mr::Renderer *renderer);
 
 int main(int argc, char *argv[])
 {
     mr::Application::RunParams params = {};
+    params.onStart = OnStart;
     params.onUpdate = OnUpdate;
     params.onRender = OnRender;
 
@@ -43,6 +132,11 @@ int main(int argc, char *argv[])
     myApp = UserApp();
     mr::Application::GetInstance().Run(params);
     return 0;
+}
+
+void OnStart()
+{
+    myApp.Start();
 }
 
 void OnUpdate()
--- src/mr_platform/mr_gl_renderer.cpp
+++ src/mr_platform/mr_gl_renderer.cpp
@@ -14,13 +14,6 @@
 
 #include "mr_model.h"
 #include "mr_mesh.h"
-#include "mr_shader.h"
-#include "mr_camera.h"
-#include "mr_vertex_layout.h"
-#include "mr_texture.h"
-#include "mr_observer_camera.h"
-#include "mr_fps_camera.h"
-#include "mr_light.h"
 
 #include <assimp/Importer.hpp>
 
@@ -31,18 +24,6 @@ struct Renderer::gui_init_info_s
 {
     char glVersion[16];
 };
-    
-static Shader *shader = nullptr;
-
-static FPSCamera cam;
-static Texture *tex = nullptr;
-static Texture *specMap = nullptr;
-
-static Light *directional = new DirectionalLight(
-    glm::vec3(1.0f, 1.0f, 1.0f), 
-    0.5f);
-
-static Model *model = nullptr;
 
 GlRenderer::GlRenderer()
 {
@@ -51,39 +32,10 @@ GlRenderer::GlRenderer()
     #endif
     glEnable(GL_DEPTH_TEST);
     glClearColor(0.23f, 0.23f, 0.23f, 1.0f);
-
-    DirectionalLight *dirLight = (DirectionalLight*)directional;
-    dirLight->direction = glm::normalize(glm::vec3(0.0f, -0.2f, 0.8f));
-
-    Shader::CreateParams shaderCreateParams = {};
-    shaderCreateParams.vertFilePath = "D:\\Documents\\git\\MobRend\\resources\\default_shader.vert.spv";
-    shaderCreateParams.fragFilePath = "D:\\Documents\\git\\MobRend\\resources\\default_shader.frag.spv";
-    shader = Shader::Create(shaderCreateParams);
-
-    cam = FPSCamera();
-    Camera::Config camConfig = {};
-    camConfig.perspective.fov = glm::radians(60.0f);
-    camConfig.perspective.aspectRatio = 1280.0f / 720.0f;
-    camConfig.perspective.near = 0.1f;
-    camConfig.perspective.far = 1000.0f;
-    cam.camera = Camera(
-        Camera::Type::PERSPECTIVE,
-        camConfig
-    );
-
-    tex = Texture::Create("D:\\Pictures\\Screenshots\\Screenshot (44).png");
-    specMap = Texture::Create("D:\\Documents\\progs\\krita_resources\\MobRend\\IU_Spec.png");
-
-    Application::GetInstance().GetMainWindow()->SetCursorVisible(false);
-
-    model = Model::Load("D:\\Documents\\Clases\\4th Year\\sem-2\\it386\\sprints\\3rd\\Lodge_Modular_Pieces_Exploded.fbx");
 }
 
 GlRenderer::~GlRenderer()
 {
-    delete(directional);
-    delete(shader);
-    delete(model);
 }
 
 void GlRenderer::SetViewport(int x, int y, int width, int height) 
@@ -93,57 +45,7 @@ void GlRenderer::SetViewport(int x, int y, int width, int height)
 
 void GlRenderer::OnRenderBegin()
 {
-    static glm::mat4 identityMat = glm::identity<glm::mat4>();
-    static glm::mat4 translated = glm::translate(identityMat, glm::vec3(1.0f, 0.5f, 10.0f));
-
-    cam.Update();
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    shader->Bind();
-
-    shader->UploadMat4(
-        "u_cam.viewProjection",
-        cam.camera.GetViewProjection()
-    );
-
-    shader->UploadMat4(
-        "u_cam.model",
-        identityMat
-    );
-
-    glm::mat3 normalMat = glm::transpose(glm::inverse(identityMat));
-    shader->UploadMat3(
-        "u_cam.normalMat",
-        normalMat
-    );
-
-    shader->UploadVec3(
-        "u_scene.color",
-        glm::vec3(1.0f, 1.0f, 1.0f)
-    );
-
-    directional->Bind(shader, "u_scene.ambientLight");
-    
-    shader->UploadVec3(
-        "u_scene.viewPos",
-        cam.camera.GetPosition()
-    );
-
-    shader->UploadVec3("u_scene.phongMaterial.diffuse", glm::vec3(1.0f, 0.5f, 0.31f));
-    shader->UploadFloat("u_scene.phongMaterial.diffuseMapStrength", 0.0f);
-    
-    shader->UploadVec3("u_scene.phongMaterial.specular", glm::vec3(0.5f, 0.5f, 0.5f));
-    shader->UploadFloat("u_scene.phongMaterial.specularMapStrength", 0.0f);
-    shader->UploadFloat("u_scene.phongMaterial.shininess", 32.f);
-
-    shader->UploadTexture2D("u_diffuseMap", tex);
-    shader->UploadTexture2D("u_specularMap", specMap);
-
-    Renderer::Command cmd = {};
-    cmd.topologyType = TopologyType::TRIANGLES;
-    cmd.model = model;
-    cmd.renderObjectType = RenderObjectType::MODEL;
-    this->Render(cmd);
 }
 
 void GlRenderer::OnRenderEnd()
