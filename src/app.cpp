eed0a322ed1f075a43c56fe1b2d0d65d6b148da6
Luis Chiang <lahoising78@gmail.com>
2021-08-02 23:45:23 -0400

    create mesh manually

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "mr_application.h"
+#include "mr_mesh.h"
 
 class UserApp : public mr::Program
 {
@@ -9,6 +10,29 @@ public:
 
     virtual void OnStart() override
     {
+        mr::VertexLayout layout = 
+        {
+            {mr::ATTRIBUTE_TYPE_FLOAT, 2}
+        };
+
+        float vertices[] = {
+            -0.5f, -0.5f,
+            0.5f, -0.5f,
+            0.0f, 0.5f
+        };
+
+        uint32_t indices[] = {
+            0, 1, 2
+        };
+
+        mr::Mesh::CreateParams meshParams = {};
+        meshParams.vertexLayout = &layout;
+        meshParams.vertices = vertices;
+        meshParams.verticesArraySize = sizeof(vertices);
+        meshParams.indexCount = sizeof(indices) / sizeof(uint32_t);
+        meshParams.indices = indices;
+
+        this->mesh = new mr::Mesh(meshParams);
     }
 
     virtual void OnUpdate() override
@@ -17,6 +41,10 @@ public:
 
     virtual void OnRender(mr::Renderer *renderer) override
     {
+        mr::Renderer::Command cmd = {};
+        cmd.mesh = this->mesh;
+        cmd.renderObjectType = mr::RENDER_OBJECT_MESH;
+        renderer->Render(cmd);
     }
 
     virtual void OnGuiRender() override
@@ -25,9 +53,11 @@ public:
 
     virtual void OnDestroy() override
     {
+        delete(this->mesh);
     }
 
 private:
+    mr::Mesh *mesh = nullptr;
 };
 
 int main(int argc, char *argv[])
